228821aff400b87cd839ab1e0e5a331c2036c9e4
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2026-04-15 17:09:58 +0300

    feat: simple routing

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ import http from 'http';
 
 const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-    console.log(req.url);
-    console.log(req.method);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({message: 'Hello World'}));
+    try {
+        if (req.method === 'GET') { 
+            if(req.url === '/') {
+                res.writeHead(200, {'Content-Type': 'application/html'});
+                res.end('<h1>Homepage</h1>');
+            }
+            else if(req.url === '/about') {
+                res.writeHead(200, {'Content-Type': 'application/html'});
+                res.end('<h1>About Page</h1>');
+            }
+            else {
+                res.writeHead(404, {'Content-Type': 'application/html'});
+                res.end('<h1>Page Not Found</h1>');
+            }
+                }
+        else {
+            throw Error('Method Not Allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'application/plain'});
+        res.end('Server Error');
+    }
+    
 });
 
 server.listen(PORT, () => {
